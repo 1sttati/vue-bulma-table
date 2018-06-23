b75972ab6d9d2c5ad1b7472b9081d32d331778a9
FST\Fst <first_100@hotmail.com>
2018-06-23 19:21:27 +0700

    fix style

--- DataTable.vue
+++ DataTable.vue
@@ -292,17 +292,18 @@ export default {
 .datatable-wrapper::-webkit-scrollbar-track {
   -webkit-box-shadow: inset 0 0 3px rgba(0, 0, 0, 0.3);
   border-radius: 10px;
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: rgba(255, 255, 255, 0.1);
 }
 
 .datatable-wrapper::-webkit-scrollbar {
   width: 8px;
-  background-color: rgba(0, 0, 0, 0.1);
+  height: 8px;
+  background-color: rgba(255, 255, 255, 0.1);
 }
 
 .datatable-wrapper::-webkit-scrollbar-thumb {
   border-radius: 10px;
   -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: rgba(255, 255, 255, 0.1);
 }
 </style>
